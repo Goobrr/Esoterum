853f27f1c00c4c95a6e3f708ced1bbf5c1ad8ec1
MEEP of Faith <meepmatthew@gmail.com>
2021-09-21 12:32:40 -0700

    Better Junction Drawing

--- src/esoterum/world/blocks/binary/BinaryJunction.java
+++ src/esoterum/world/blocks/binary/BinaryJunction.java
@@ -1,7 +1,14 @@
 package esoterum.world.blocks.binary;
 
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import mindustry.graphics.*;
+
 // too similar to BinaryRouter?
 public class BinaryJunction extends BinaryBlock{
+    public TextureRegion[] directionRegion = new TextureRegion[2];
+
     public BinaryJunction(String name){
         super(name);
         emits = true;
@@ -10,9 +17,28 @@ public class BinaryJunction extends BinaryBlock{
         outputs = new boolean[]{true, true, true, true};
     }
 
-    public class BinaryJunctionBuild extends BinaryBuild {
+    @Override
+    public void load(){
+        super.load();
+
+        for(int i = 0; i < 2; i++){
+            directionRegion[i] = Core.atlas.find(name + "-direction-" + i);
+        }
+    }
+
+    @Override
+    protected TextureRegion[] icons(){
+        return new TextureRegion[]{
+            region,
+            topRegion,
+            directionRegion[0],
+            directionRegion[1]
+        };
+    }
+
+    public class BinaryJunctionBuild extends BinaryBuild{
         @Override
-        public void updateTile() {
+        public void updateTile(){
             super.updateTile();
             lastSignal = false;
             for(BinaryBuild b : nb){
@@ -21,19 +47,32 @@ public class BinaryJunction extends BinaryBlock{
         }
 
         @Override
-        public boolean signalFront() {
+        public void drawConnections(){
+
+            Draw.color(Color.white, Pal.accent, signalFront() || signalBack() ? 1f : 0f);
+            Draw.rect(directionRegion[0], x, y);
+            Draw.color(Color.white, Pal.accent, signalLeft() || signalRight() ? 1f : 0f);
+            Draw.rect(directionRegion[1], x, y);
+            Draw.color(Color.white, Pal.accent, lastSignal ? 1f : 0f);
+        }
+
+        @Override
+        public boolean signalFront(){
             return getSignal(nb.get(2), this);
         }
+
         @Override
-        public boolean signalBack() {
+        public boolean signalBack(){
             return getSignal(nb.get(0), this);
         }
+
         @Override
-        public boolean signalLeft() {
+        public boolean signalLeft(){
             return getSignal(nb.get(3), this);
         }
+
         @Override
-        public boolean signalRight() {
+        public boolean signalRight(){
             return getSignal(nb.get(1), this);
         }
     }
